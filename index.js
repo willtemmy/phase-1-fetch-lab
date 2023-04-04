4df75066029beb685dbe211fddc180f30577948b
William Mtemi Mponzi <willbrian009@gmail.com>
2023-04-04 22:29:26 +0300

    completee

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(bookData => bookData.forEach(book => renderBooks(book)))
 }
 
 function renderBooks(books) {
